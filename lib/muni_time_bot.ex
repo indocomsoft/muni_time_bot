5bf4f59e676e2d0932276e453df0fc09347e6cf1
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-06-05 23:01:41 -0700

    Remove placeholder code

--- lib/muni_time_bot.ex
+++ lib/muni_time_bot.ex
@@ -2,17 +2,4 @@ defmodule MuniTimeBot do
   @moduledoc """
   Documentation for MuniTimeBot.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MuniTimeBot.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/muni_time_bot_test.exs
+++ test/muni_time_bot_test.exs
@@ -1,8 +1,3 @@
 defmodule MuniTimeBotTest do
   use ExUnit.Case
-  doctest MuniTimeBot
-
-  test "greets the world" do
-    assert MuniTimeBot.hello() == :world
-  end
 end
